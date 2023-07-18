823a909bdc6b4c53806e90f1e648b2da7ad1586b
Dipti Sengupta <diptisengupta@Diptis-Own-MacBook.local>
2023-07-18 11:10:05 +0200

    Changes till 18 Jul

--- HPO/ray_cluster_test/BoHBCode/node_worker.py
+++ HPO/ray_cluster_test/BoHBCode/node_worker.py
@@ -1,18 +1,22 @@
 import logging
 logging.basicConfig(level=logging.INFO)
-
+import numpy
 import argparse
-import os
+import pickle
+import time
+import torch
+
 import hpbandster.core.nameserver as hpns
 import hpbandster.core.result as hpres
 
 from hpbandster.optimizers import BOHB as BOHB
-import numpy
-import time
+from hpbandster.examples.commons import MyWorker
 
-import ConfigSpace as CS
 from hpbandster.core.worker import Worker
 import socket
+import os
+import ConfigSpace as CS
+
 
 class MyWorker(Worker):
 
@@ -42,6 +46,11 @@ class MyWorker(Worker):
         print(f"Hostname: {socket.gethostname()}")
         print(f"Process ID: {os.getpid()}")
 
+        if torch.cuda.is_available():
+            print(f'Number of devices: {torch.cuda.device_count()}')
+        else:
+            print('No GPU available.')
+
         res = numpy.clip(config['x'] + numpy.random.randn()/budget, config['x']/2, 1.5*config['x'])
         time.sleep(self.sleep_interval)
 
@@ -57,52 +66,62 @@ class MyWorker(Worker):
         return(config_space)
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Example 3 - Local and Parallel Execution.')
+    parser = argparse.ArgumentParser(description='Example 1 - sequential and local execution.')
     parser.add_argument('--min_budget', type=float, help='Minimum budget used during the optimization.', default=9)
     parser.add_argument('--max_budget', type=float, help='Maximum budget used during the optimization.', default=243)
     parser.add_argument('--n_iterations', type=int, help='Number of iterations performed by the optimizer', default=4)
     parser.add_argument('--n_workers', type=int, help='Number of workers to run in parallel.', default=2)
     parser.add_argument('--worker', help='Flag to turn this into a worker process', action='store_true')
+    parser.add_argument('--run_id', type=str,
+                        help='A unique run id for this optimization run. An easy option is to use the job id of the clusters scheduler.',
+                        default='running_Laptop')
+    parser.add_argument('--nic_name', type=str, help='Which network interface to use for communication.', default='lo0')
+    parser.add_argument('--shared_directory', type=str,
+                        help='A directory that is accessible for all processes, e.g. a NFS share.', default=".")
+    parser.add_argument('--task_id', type=int, help='Task ID')
 
     args = parser.parse_args()
 
+    # Every process has to lookup the hostname
+    host = hpns.nic_name_to_host(args.nic_name)
+    print(f"Host: {host}")
+
     if args.worker:
-        w = MyWorker(sleep_interval=0.5, nameserver='127.0.0.1', run_id='example3')
+        time.sleep(5)  # short artificial delay to make sure the nameserver is already running
+        w = MyWorker(sleep_interval=0.5, run_id=args.run_id, host=host)
+        w.load_nameserver_credentials(working_directory=args.shared_directory)
         w.run(background=False)
         exit(0)
 
-    # Start a nameserver (see example_1)
-    NS = hpns.NameServer(run_id='example3', host='127.0.0.1', port=None)
-    NS.start()
+    # Start a nameserver:
+    # We now start the nameserver with the host name from above and a random open port (by setting the port to 0)
+    NS = hpns.NameServer(run_id=args.run_id, host=host, port=0, working_directory=args.shared_directory)
+    ns_host, ns_port = NS.start()
 
-    # Run an optimizer (see example_2)
+    # Most optimizers are so computationally inexpensive that we can affort to run a
+    # worker in parallel to it. Note that this one has to run in the background to
+    # not plock!
+    w = MyWorker(sleep_interval=0.5, run_id=args.run_id, host=host, nameserver=ns_host, nameserver_port=ns_port)
+    w.run(background=True)
+
+    # Run an optimizer
+    # We now have to specify the host, and the nameserver information
     bohb = BOHB(configspace=MyWorker.get_configspace(),
-                run_id='example3',
+                run_id=args.run_id,
+                host=host,
+                nameserver=ns_host,
+                nameserver_port=ns_port,
                 min_budget=args.min_budget, max_budget=args.max_budget
                 )
     res = bohb.run(n_iterations=args.n_iterations, min_n_workers=args.n_workers)
+
+    # In a cluster environment, you usually want to store the results for later analysis.
+    # One option is to simply pickle the Result object
+    with open(os.path.join(args.shared_directory, 'results.pkl'), 'wb') as fh:
+        pickle.dump(res, fh)
 
     # Step 4: Shutdown
     # After the optimizer run, we must shutdown the master and the nameserver.
     bohb.shutdown(shutdown_workers=True)
     NS.shutdown()
-
-    # Step 5: Analysis
-    # Each optimizer returns a hpbandster.core.result.Result object.
-    # It holds informations about the optimization run like the incumbent (=best) configuration.
-    # For further details about the Result object, see its documentation.
-    # Here we simply print out the best config and some statistics about the performed runs.
-    id2config = res.get_id2config_mapping()
-    incumbent = res.get_incumbent_id()
-
-    all_runs = res.get_all_runs()
-
-    print('Best found configuration:', id2config[incumbent]['config'])
-    print('A total of %i unique configurations where sampled.' % len(id2config.keys()))
-    print('A total of %i runs where executed.' % len(res.get_all_runs()))
-    print('Total budget corresponds to %.1f full function evaluations.' % (
-                sum([r.budget for r in all_runs]) / args.max_budget))
-    print('Total budget corresponds to %.1f full function evaluations.' % (
-                sum([r.budget for r in all_runs]) / args.max_budget))
-    print('The run took  %.1f seconds to complete.' % (
-                all_runs[-1].time_stamps['finished'] - all_runs[0].time_stamps['started']))
+    print('End of worker')
--- HPO/ray_cluster_test/BoHBCode/process_script.sh
+++ HPO/ray_cluster_test/BoHBCode/process_script.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=DDG
 #SBATCH -p mlhiwidlc_gpu-rtx2080-advanced
 
-#SBATCH -c 1 # number of cores
+#SBATCH --gres=gpu:4 # I only want 4 gpus per node right now var will be SLURM_GPUS_ON_NODE
 #SBATCH -a 0-1 # array size
 #SBATCH --time=00:5:00
 #SBATCH --mail-user=dipti.sengupta@students.uni-freiburg.de
 #SBATCH --mail-type=BEGIN,END,FAIL
-#SBATCH --output=/work/dlclarge1/dsengupt-zap_hpo_og/logs/process-%A-%a.out
-#SBATCH --error=/work/dlclarge1/dsengupt-zap_hpo_og/logs/process-%A-%a.error
+#SBATCH --output=/work/dlclarge1/dsengupt-zap_hpo_og/logs/node-%A-%a.out
+#SBATCH --error=/work/dlclarge1/dsengupt-zap_hpo_og/logs/node-%A-%a.error
 
 
 echo "Activate environment for Job ID $SLURM_JOB_ID"
@@ -18,6 +18,13 @@ source ~/tinybert_nlp/bin/activate
 export TOKENIZERS_PARALLELISM=False
 cd $(ws_find zap_hpo_og)/TinyBert/HPO/ray_cluster_test/BoHBCode
 
+if [ $SLURM_ARRAY_TASK_ID -eq 0 ];
+   then srun python3 node_worker.py --run_id $SLURM_JOB_NAME --nic_name eth0 --shared_directory ./BOHB_Results --task_id $SLURM_ARRAY_TASK_ID
+else
+   srun python3 node_worker.py --run_id $SLURM_JOB_NAME --nic_name eth0  --shared_directory ./BOHB_Results --task_id $SLURM_ARRAY_TASK_ID --worker
+fi
+
+echo 'End of Script'
 
 
 
