720522d200e72bd749f3df3c17cd48dcfbac5078
Dipti Sengupta <diptisengupta@Diptis-Own-MacBook.local>
2023-09-17 10:25:03 +0200

    Sept 17 10.30 changes

--- HPO/ray_cluster_test/asha_ray_transformers.py
+++ HPO/ray_cluster_test/asha_ray_transformers.py
@@ -21,7 +21,15 @@ from ray.tune import Callback
 from ray.tune.experiment import Trial
 from ray.tune.integration.pytorch_lightning import TuneReportCallback
 from ray.tune.schedulers import ASHAScheduler
+import logging
+import sys
 
+# Ray Train report callback
+from ray import train
+import tempfile
+import shutil
+from ray._private.usage.usage_lib import TagKey, record_extra_usage_tag
+from ray.train import Checkpoint
 # local imports
 try:
     from BoHBCode.data_modules import OmpData, get_datamodule
@@ -29,7 +37,7 @@ try:
 except ImportError:
     from .BoHBCode.data_modules import OmpData, get_datamodule
     from .BoHBCode.train_module import AshaTransformer
-
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 class MyCallback(Callback):
     def on_trial_result(self, iteration, trials, trial, result, **info):
@@ -38,6 +46,43 @@ class MyCallback(Callback):
     def on_trial_error(self, iteration: int, trials: List["Trial"], trial: "Trial", **info):
         print(f"Got error for {trial.trainable_name} with config {trial.config}")
 
+
+class RayTrainReportCallback(Callback):
+    """A simple callback that reports checkpoints to Ray on train epoch end."""
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.trial_name = train.get_context().get_trial_name()
+        self.local_rank = train.get_context().get_local_rank()
+        self.tmpdir_prefix = os.path.join(tempfile.gettempdir(), self.trial_name)
+        if os.path.isdir(self.tmpdir_prefix) and self.local_rank == 0:
+            shutil.rmtree(self.tmpdir_prefix)
+
+        record_extra_usage_tag(TagKey.TRAIN_LIGHTNING_RAYTRAINREPORTCALLBACK, "1")
+
+    def on_train_epoch_end(self, trainer, pl_module) -> None:
+        # Creates a checkpoint dir with fixed name
+        tmpdir = os.path.join(self.tmpdir_prefix, str(trainer.current_epoch))
+        os.makedirs(tmpdir, exist_ok=True)
+
+        # Fetch metrics
+        metrics = trainer.callback_metrics
+        metrics = {k: v.item() for k, v in metrics.items()}
+
+        # (Optional) Add customized metrics
+        metrics["epoch"] = trainer.current_epoch
+        metrics["step"] = trainer.global_step
+
+        # Save checkpoint to local
+        ckpt_path = os.path.join(tmpdir, "checkpoint.ckpt")
+        trainer.save_checkpoint(ckpt_path, weights_only=False)
+
+        # Report to train session
+        checkpoint = Checkpoint.from_directory(tmpdir)
+        train.report(metrics=metrics, checkpoint=checkpoint)
+
+        if self.local_rank == 0:
+            shutil.rmtree(tmpdir)
 
 hpo_config = {
     'model_name_or_path': tune.choice(["bert-base-uncased", "bert-base-multilingual-cased",
@@ -67,16 +112,16 @@ hpo_config = {
 # https://github.com/ray-project/ray/issues/37394
 # RAY TUNE 3.0 WITH TORCH TRAINER
 
-def objective_torch_trainer(config):
-    print(f"config-------> {config}")
+def objective_torch_trainer(config, data_dir=os.path.join(os.getcwd(), "testing_data")):
+    logging.debug(f"config-------> {config} in dir {data_dir} and cwd {os.getcwd()}")
     dm = get_datamodule(task_name="sentilex", model_name_or_path=config['model_name_or_path'],
                         max_seq_length=config['max_seq_length'],
                         train_batch_size=config['per_device_train_batch_size'],
-                        eval_batch_size=config['per_device_eval_batch_size'])
+                        eval_batch_size=config['per_device_eval_batch_size'], data_dir=data_dir)
     dm.setup("fit")
-    model = AshaTransformer(config=config, num_labels=dm.task_metadata['num_labels'], dataset_name="omp")
+    model = AshaTransformer(config=config, num_labels=dm.task_metadata['num_labels'])
     ckpt_report_callback = RayTrainReportCallback()
-    log_dir = os.path.join(os.getcwd(), "ray_results/torch_trainer_logs")
+    log_dir = os.path.join(os.getcwd(), "ray_results_log/torch_trainer_logs")
     print(f"log_dir-----> {log_dir}")
     trainer = pl.Trainer(
         max_epochs=config['num_epochs'],
@@ -86,7 +131,6 @@ def objective_torch_trainer(config):
         # If fractional GPUs passed in, convert to int.
         devices='auto',
         accelerator='auto',
-        num_nodes=1,
         enable_progress_bar=True,
         max_time="00:12:00:00",  # give each run a time limit
         val_check_interval=0.5,  # check validation set 4 times during a training epoch
@@ -130,8 +174,9 @@ def tune_func_torch_trainer(num_samples=10, num_epochs=10, exp_name="torch_trans
         )
     # A `RunConfig` was passed to both the `Tuner` and the `TorchTrainer`.
     # The run config passed to the `Tuner` is the one that will be used
+    train_fn_with_parameters = tune.with_parameters(objective_torch_trainer, data_dir=os.path.join(os.getcwd(), "testing_data"))
     ray_trainer = TorchTrainer(
-        objective_torch_trainer,
+        train_fn_with_parameters,
         scaling_config=scaling_config,
         run_config=ray.train.RunConfig(
             checkpoint_config=ray.train.CheckpointConfig(
@@ -142,7 +187,74 @@ def tune_func_torch_trainer(num_samples=10, num_epochs=10, exp_name="torch_trans
         )
     )
 
-    result_dir = os.path.join(os.getcwd(), "ray_results")
+    result_dir = os.path.join(os.getcwd(), "ray_results_result")
+    logging.debug(f"result_dir-----> {result_dir}")
+    restore_path = os.path.join(result_dir, exp_name)
+    tuner = tune.Tuner(ray_trainer,
+                       tune_config=tune.TuneConfig(
+                           metric="ptl/val_accuracy",
+                           mode="max",
+                           scheduler=scheduler,
+                           num_samples=num_samples,
+                           trial_name_creator=trial_dir_name,
+                           trial_dirname_creator=trial_dir_name,
+                       ),
+                       run_config=ray.train.RunConfig(
+                           name=exp_name,
+                           verbose=2,
+                           storage_path=result_dir,
+                           log_to_file=True,
+                           checkpoint_config=ray.train.CheckpointConfig(
+                               num_to_keep=3,
+                               checkpoint_score_attribute="ptl/val_accuracy",
+                               checkpoint_score_order="max",
+                           ),
+                       ),
+                       param_space={"train_loop_config": hpo_config},
+                       )
+    results = tuner.fit()
+
+    print("Best hyperparameters found were: ", results.get_best_result().config)
+
+
+def debug_torch(num_samples=10, num_epochs=10, exp_name="debug_torch"):
+    scheduler = ASHAScheduler(max_t=num_epochs,
+                              grace_period=1,
+                              reduction_factor=2)
+
+    if torch.cuda.is_available():
+        accelerator = 'gpu'
+        use_gpu = True
+        gpus_per_worker = 4
+        # each Trainer gets that.
+        scaling_config = ray.train.ScalingConfig(
+            # no of other nodes?
+            num_workers=gpus_per_worker, use_gpu=use_gpu, resources_per_worker={"CPU": 2, "GPU": 1}
+        )
+    else:
+        accelerator = 'cpu'
+        use_gpu = False
+        scaling_config = ray.train.ScalingConfig(
+            # no of other nodes?
+            num_workers=5, use_gpu=use_gpu, resources_per_worker={"CPU": 1, }
+        )
+    # A `RunConfig` was passed to both the `Tuner` and the `TorchTrainer`.
+    # The run config passed to the `Tuner` is the one that will be used
+    train_fn_with_parameters = tune.with_parameters(objective_torch_trainer,
+                                                    data_dir=os.path.join(os.getcwd(), "debug_testing_data"))
+    ray_trainer = TorchTrainer(
+        train_fn_with_parameters,
+        scaling_config=scaling_config,
+        run_config=ray.train.RunConfig(
+            checkpoint_config=ray.train.CheckpointConfig(
+                num_to_keep=3,
+                checkpoint_score_attribute="ptl/val_accuracy",
+                checkpoint_score_order="max",
+            ),
+        )
+    )
+
+    result_dir = os.path.join(os.getcwd(), "ray_results_result")
     print(f"result_dir-----> {result_dir}")
     tuner = tune.Tuner(ray_trainer,
                        tune_config=tune.TuneConfig(
@@ -182,8 +294,8 @@ def tune_function(num_samples=10, num_epochs=10, exp_name="tune_transform"):
     train_fn_with_parameters = tune.with_parameters(objective_func, data_dir=os.path.join(os.getcwd(), "testing_data"))
     gpus_per_trial = 2 if torch.cuda.is_available() else 0
     resources_per_trial = {"cpu": 2, }
-
-    result_dir = os.path.join(os.getcwd(), "ray_results")
+    # saved correctly
+    result_dir = os.path.join(os.getcwd(), "ray_results_vanilla")
     tuner = tune.Tuner(
         tune.with_resources(
             train_fn_with_parameters,
@@ -224,6 +336,7 @@ def objective_func(config, data_dir='data'):
             "mean_accuracy": "ptl/val_accuracy"
         },
         on="validation_end")
+    log_dir = os.path.join(os.getcwd(), "ray_results_log/torch_trainer_logs")
     trainer = pl.Trainer(
         max_epochs=config['num_epochs'],
         # If fractional GPUs passed in, convert to int.
@@ -231,7 +344,7 @@ def objective_func(config, data_dir='data'):
         accelerator='auto',
         num_nodes=1,
         logger=TensorBoardLogger(
-            save_dir=os.getcwd(), name="SentiLex150", version="."),
+            save_dir=log_dir),
         enable_progress_bar=True,
         callbacks=[tune_callback])
     trainer.fit(model, dm)
@@ -306,14 +419,16 @@ if __name__ == "__main__":
     # objective_func(sample_config)
     # objective_torch_trainer(sample_config)
 
+    # debug_torch(num_samples=10, num_epochs=4, exp_name="debug_torch_transform")
 
+    # Start training Main
     if args.smoke_test:
         print("Smoketesting...")
         tune_func_torch_trainer(num_samples=2, num_epochs=4, exp_name=args.exp_name)
     else:
-        tune_func_torch_trainer(num_samples=15, num_epochs=4, exp_name=args.exp_name)
+        tune_func_torch_trainer(num_samples=150, num_epochs=4, exp_name=args.exp_name)
 
-    # Start training
+
     # if args.smoke_test:
     #     print("Smoketesting...")
     #     tune_function(num_samples=2, num_epochs=4, exp_name=args.exp_name)
